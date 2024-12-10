2b9b889642795f570e20c22b12ba7d5ff3d63d04
HassanAbukar <xasaani.abukar@gmail.com>
2024-12-10 10:14:44 +0300

    math program

---
+++ Reactangle area cal/app.py
@@ -0,0 +1,9 @@
+# area reactangle calculation
+
+length = float(input("Enter the length"))
+width = float(input("Enter the width"))
+
+
+area = length * width
+
+print(f"Reactangle area is {area} ")
---
+++ circumstance of the circle/app.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+circumstance = 2 * math.pi * radius
+
+print(f"The circumstance of the circle is: {round(circumstance, 2)} cm")
